6f9ac4ce49b10e39a13530c77e70b019ab41575e
intern <oleksandrch@tahometer.com>
2019-07-23 09:33:58 +0300

    init commit

---
+++ src/main/java/com/todo/some/SomeApplication.java
@@ -0,0 +1,13 @@
+package com.todo.some;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomeApplication.class, args);
+	}
+
+}
