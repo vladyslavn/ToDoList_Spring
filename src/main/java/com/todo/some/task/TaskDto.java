1c89c939e306c9cbfa5aa6a509291c5afcbe6fb0
intern <oleksandrch@tahometer.com>
2019-07-26 09:49:45 +0300

    Created TaskDto class

---
+++ src/main/java/com/todo/some/task/TaskDto.java
@@ -0,0 +1,41 @@
+package com.todo.some.task;
+
+public class TaskDto {
+
+    private int id;
+    private int listId;
+    private String name;
+    private boolean isDone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getListId() {
+        return listId;
+    }
+
+    public void setListId(int listId) {
+        this.listId = listId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+}
